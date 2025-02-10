0ffa9d4a69e2e2a91fccc4737bdc664938941892
Kacper Cholewiński <kacpercholewinski03@gmail.com>
2025-02-10 12:46:59 +0100

    exercise 13.5

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -15,13 +19,16 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.ebay.com/");
 
-        WebElement categoryDropdown = driver.findElement(By.xpath("//div[@id=\"gh-search-box\"]/div/select")); // [4]
+        WebElement categoryDropdown = driver.findElement(By.cssSelector("#gh-search-box > div > select")); // [4]
         Select categorySelect = new Select(categoryDropdown);
         categorySelect.selectByValue("293");
-        WebElement searchBox = driver.findElement(By.xpath("//input[@id=\"gh-ac\"]"));
+        WebElement searchBox = driver.findElement(By.id("gh-ac"));
         searchBox.sendKeys("mavic mini");
         searchBox.submit();
-//        inputField.sendKeys("Laptop");
-//        inputField.submit();
+        new WebDriverWait(driver, 20).until(ExpectedConditions.numberOfElementsToBeMoreThan(By.className("s-item"), 0));
+        List<WebElement> elements = driver.findElements(By.className("s-item"));
+        elements.stream()
+                .map(element -> element.findElement(By.cssSelector(".s-item__wrapper > .s-item__info > .s-item__link > .s-item__title > span")))
+                .forEach(element -> System.out.println(element.getText()));
     }
 }
