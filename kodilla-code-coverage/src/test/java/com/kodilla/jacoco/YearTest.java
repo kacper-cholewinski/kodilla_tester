167f085d6a77e47e636a459b556fe37530a425b8
Kacper Cholewiński <kacpercholewinski03@gmail.com>
2025-02-25 12:58:32 +0100

    exercise 15.2

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -0,0 +1,61 @@
+package com.kodilla.jacoco;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class YearTest {
+
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy400() {
+
+        //given
+        Year year = new Year(1600);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy100() {
+
+        //given
+        Year year = new Year(1400);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfNotDivisibleBy4() {
+
+        //given
+        Year year = new Year(2025);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldBeLeapYearIfNotDivisibleBy100() {
+
+        //given
+        Year year = new Year(2024);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+}
